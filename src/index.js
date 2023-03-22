8f17af006c56ef82f8821981afa9d9bfbbb2ccea
Rahul Mahato <mahatorahul025@gmail.com>
2023-03-22 19:17:20 +0530

    completed nodemailer setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const { PORT } = require("./configs/serverConfig");
+const { sendBasicEmail } = require("./services/email-service");
 
 const setupAndStartServer = () => {
   const app = express();
+
+  //   sendBasicEmail(
+  //     "notification.center@admin.com",
+  //     "mahatorahul025@gmail.com",
+  //     "Flight Booking Details",
+  //     "Your Flight has been booked successfully. Enjoy!"
+  //   );
 
   app.listen(PORT, () => {
     console.log(`Started server on PORT ${PORT}`);
